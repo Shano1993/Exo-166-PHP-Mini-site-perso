2073e77935855ff9c02ab47b5cb67d1b75fa1993
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-26 12:18:56 +0100

    site

--- pages/contact.php
+++ pages/contact.php
@@ -1,3 +1,22 @@
 <section>
     <h1>Contact</h1>
+    <form action="/public/save.php" method="post">
+        <div>
+            <label for="id-name"></label>
+            <input type="text" id="id-name" name="name" placeholder="Name">
+        </div>
+        <div>
+            <label for="id-firstname"></label>
+            <input type="text" id="id-firstname" name="firstname" placeholder="First Name">
+        </div>
+        <div>
+            <label for="id-mail"></label>
+            <input type="email" id="id-mail" name="mail" placeholder="Mail">
+        </div>
+        <div>
+            <label for="id-message"></label>
+            <textarea name="message" id="id-message" cols="30" rows="10" placeholder="Enter your message"></textarea>
+        </div>
+        <input type="submit" id="id-submit" name="submit" value="Send">
+    </form>
 </section>
